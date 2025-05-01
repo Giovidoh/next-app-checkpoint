e63631c48434f1353fdc91ca10f1a3879e9965e8
giovanni <giovidoh@gmail.com>
2025-05-01 20:43:39 +0000

    feat: Sonner added & Providers tags wrapped over children in layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
+import { Toaster } from "sonner";
+import Providers from "@/providers/Providers";
+
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -27,7 +30,17 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                {children}
+                <Providers>{children}</Providers>
+                <Toaster
+                    toastOptions={{
+                        style: {
+                            fontSize: "16px",
+                        },
+                    }}
+                    duration={10000}
+                    closeButton
+                    richColors
+                />
             </body>
         </html>
     );

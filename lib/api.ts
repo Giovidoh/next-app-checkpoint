1a48a75092ed1c3d0ad12513a19b651a1e9a7791
Giovidoh <giovidoh@gmail.com>
2025-08-19 23:58:34 +0000

    chore: Refactor API function to set default prefix and streamline error handling

--- lib/api.ts
+++ lib/api.ts
@@ -5,56 +5,55 @@ const proxyRoute = "/api/proxy";
 const API_PREFIX = "/api/v1";
 
 interface ApiOptions {
-    prefix?: string;
-    method?: string;
-    body?: any;
-    token?: string;
-    withCredentials?: boolean;
+  prefix?: string;
+  method?: string;
+  body?: any;
+  token?: string;
+  withCredentials?: boolean;
 }
 
 export const api = async <T>(
-    endpoint: string,
-    {
-        prefix = "",
-        method = "GET",
-        body,
-        token,
-        withCredentials = false,
-    }: ApiOptions = {}
+  endpoint: string,
+  {
+    prefix = API_PREFIX,
+    method = "GET",
+    body,
+    token,
+    withCredentials = false,
+  }: ApiOptions = {}
 ): Promise<T> => {
-    try {
-        const isFormData = body instanceof FormData;
+  try {
+    const isFormData = body instanceof FormData;
 
-        const response = await axios({
-            method,
-            url: `${proxyRoute}${prefix}${endpoint}`,
-            headers: {
-                ...(token ? { Authorization: `Bearer ${token}` } : {}),
-                ...(isFormData ? {} : { "Content-Type": "application/json" }),
-            },
-            data: body || undefined,
-            withCredentials,
-        });
+    const response = await axios({
+      method,
+      url: `${proxyRoute}${prefix}${endpoint}`,
+      headers: {
+        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+        ...(isFormData ? {} : { "Content-Type": "application/json" }),
+      },
+      data: body || undefined,
+      withCredentials,
+    });
 
-        return response.data;
-    } catch (error: any) {
-        if (error.message.includes("Network Error")) {
-            throw "networkError";
-        }
-
-        throw error.response?.data || error;
+    return response.data;
+  } catch (error: any) {
+    if (error.message.includes("Network Error")) {
+      throw "networkError";
     }
+
+    throw error.response?.data || error;
+  }
 };
 
 export const apiLogin = (
-    data: LoginMutation
+  data: LoginMutation
 ): Promise<{ user: UserType; token: TokenData }> =>
-    api("/auth/login", { prefix: API_PREFIX, method: "POST", body: data });
+  api("/auth/login", { method: "POST", body: data });
 
 export const currentUser = (token: string): Promise<{ user: UserType }> =>
-    api("/auth/current-user", {
-        prefix: API_PREFIX,
-        method: "POST",
-        body: undefined,
-        token: token,
-    });
+  api("/auth/current-user", {
+    method: "POST",
+    body: undefined,
+    token: token,
+  });
